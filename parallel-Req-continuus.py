f7d54f58a69ea35b51c0a759398bb24dd4a41f50
promethean-lucky <62021162+lucky-promethean@users.noreply.github.com>
2020-08-21 22:11:28 -0400

    Update parallel-Req-continuus.py

--- parallel-Req-continuus.py
+++ parallel-Req-continuus.py
@@ -3,19 +3,19 @@ def para(r1, r2):
         return(r1*r2/r1+r2)
 
 print("enter parallel resistances")
-r1=float(input("r1: "))
-r2=float(input("r2: "))
+r1=float(input("R1: "))
+r2=float(input("R2: "))
 print ("Req= ",r1*r2/(r1+r2))
 req=float(r1*r2/(r1+r2))
-print ("req= ", req)
+print ("Req= ", req)
 print(" ")
 inp=1
 while (inp == 1):
         r1 = req
-        print ("r1=previous req= ",r1)
-        r2=float(input("r2: "))
+        print ("R1=previous Req= ",r1)
+        r2=float(input("R2: "))
         req=float(r1*r2/(r1+r2))
-        print ("req= ", req)
-        inp=int(input("press 1 for another comparison, >
+        print ("Req= ", req)
+        inp=int(input("press 1 for another comparison, Req taken as R1 or 0 for a clear...  \n"))
 if (inp == 0):
         quit()
